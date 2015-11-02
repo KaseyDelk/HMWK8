17fdbf74ae2a2cd3d05edc86676ead73c743fcc7
Kasey Delk <delkk@student.swosu.edu>
2015-11-02 10:04:58 -0600

    saved changes to block comment

--- src/RandomCharacter.java
+++ src/RandomCharacter.java
@@ -13,8 +13,12 @@ public class RandomCharacter {
 	
 	/* I am commenting out the code that I wrote myself to ensure that
 		* my code is correct.
-		* //Get computer to pull a random character
+		* 
+		* Code:
+		* 
+		* //Get computer to generate a random character
 		* char randomChar = (char)((int)'A' + Math.random() * ((int)'Z'-(int)'A'+1));
+		* 
 		* //Display characters for user
 		* System.out.println("Your random letter is : " + randomChar);
 		*/
