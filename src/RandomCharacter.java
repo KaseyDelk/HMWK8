50ac815d22057f3bde2ba207f55ec63d254861c3
Kasey Delk <delkk@student.swosu.edu>
2015-11-02 09:18:56 -0600

    wrote code with Dr. Evert switched to dev branch to play with code.

--- src/RandomCharacter.java
+++ src/RandomCharacter.java
@@ -1,11 +1,20 @@
 public class RandomCharacter {
 	public static void main(String[] args) {
-
-		//Get computer to pull a random character
-		char randomChar = (char)((int)'A' + Math.random() * ((int)'Z'-(int)'A'+1));
-		
-		//Display characters for user
-		System.out.println("Your random letter is : " + randomChar);
-		
+	
+		System.out.println("Hello.");
 	}
 }
+		
+	
+	
+	
+	
+	
+	
+	/* I am commenting out the code that I wrote myself to ensure that
+		* my code is correct.
+		* //Get computer to pull a random character
+		* char randomChar = (char)((int)'A' + Math.random() * ((int)'Z'-(int)'A'+1));
+		* //Display characters for user
+		* System.out.println("Your random letter is : " + randomChar);
+		*/
